218bab9dad2660607d5bd633a85cd4370343a503
Mariano Mariano <marianofheller@gmail.com>
2017-11-05 10:05:13 -0300

    dom

--- converter.js
+++ converter.js
@@ -28,9 +28,19 @@ const mwParser = function ( req, res, next) {
         const conversion = conversions.find( (conversion) => input.includes(conversion.units[0]) || input.includes(conversion.units[1]));
         const indexUnitInput = conversion.units.findIndex( (unit) => input.includes(unit) );
         const splitted = input.split(conversion[indexUnitInput]).filter( (substr) => substr );
-        if ( splitted.length > 1 && !isNumeric(splitted[0]) ) res.send("invalid unit");
+        // falta checkear que no haya numero y rellenarlo con 1
+        if ( splitted.length > 1 ) res.send("invalid unit");
+        if ( splitted.length === 0 ) splitted[0] = '1';
+        const parsedInput = splitted[0].split('/').reduce( (acc, d, i) => {
+            if ( i === 0) acc = parseFloat(d);
+            return acc;
+        }, 0 );
+        if ( !isNumeric(parsedInput) ) res.send("invalid unit");
         else {
-
+            req.conversion = {
+                initNum: parsedInput, 
+                initUnit: conversion.units[indexUnitInput],
+            }
             next();
         }
     }
@@ -42,7 +52,7 @@ const mwConverter = function( req, res, next) {
 
 	
 	req.output = {
-		input: req.query.input,
+		...req.conversion,
 	};
 	next();
 }
@@ -50,8 +60,7 @@ const mwConverter = function( req, res, next) {
 
 
 
-function isNumeric(string) {
-    
+function isNumeric(n) {
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
